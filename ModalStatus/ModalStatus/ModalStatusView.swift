ffa1bc0f8dbdb1d94328798b1f2d1eb357e8c7c2
Carlos Cortés <ccortes@gmv.com>
2017-11-23 11:03:14 +0100

    Set functions for updating view

--- ModalStatus/ModalStatus/ModalStatusView.swift
+++ ModalStatus/ModalStatus/ModalStatusView.swift
@@ -8,11 +8,53 @@
 
 import UIKit
 
-class ModalStatusView: UIView {
+public class ModalStatusView: UIView {
 
     @IBOutlet private weak var statusImage: UIImageView!
     @IBOutlet private weak var headlineLabel: UILabel!
     @IBOutlet private weak var subheadLabel: UILabel!
     
+    let nibName = "ModalStatusView"
+    var contentView: UIView!
+    
+    //MARK: Set up view
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpView()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setUpView()
+    }
+    
+    private func setUpView() {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: self.nibName, bundle: bundle)
+        self.contentView = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        addSubview(contentView)
+        
+        contentView.center = self.center
+        contentView.autoresizingMask = []
+        contentView.translatesAutoresizingMaskIntoConstraints = true
+        
+        headlineLabel.text = ""
+        subheadLabel.text = ""
+    }
+    
+    // Provide functions to update view
+    
+    public func set(image: UIImage) {
+        self.statusImage.image = image
+    }
+    
+    public func set(headline text: String) {
+        self.headlineLabel.text = text
+    }
+    
+    public func set(subhead text: String) {
+        self.subheadLabel.text = text
+    }
     
 }
