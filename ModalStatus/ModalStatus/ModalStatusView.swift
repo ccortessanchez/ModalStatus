ef5831b46b1e1b0122e2ddf594de4ee704331466
Carlos Cortés <ccortes@gmv.com>
2017-11-23 10:54:38 +0100

    Add outlets to view class

---
+++ ModalStatus/ModalStatus/ModalStatusView.swift
@@ -0,0 +1,18 @@
+//
+//  ModalStatusView.swift
+//  ModalStatus
+//
+//  Created by Carlos Cortés Sánchez on 23/11/2017.
+//  Copyright © 2017 Carlos Cortés Sánchez. All rights reserved.
+//
+
+import UIKit
+
+class ModalStatusView: UIView {
+
+    @IBOutlet private weak var statusImage: UIImageView!
+    @IBOutlet private weak var headlineLabel: UILabel!
+    @IBOutlet private weak var subheadLabel: UILabel!
+    
+    
+}
