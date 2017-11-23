ae0f280a9bd30d014df9473a96eb18fae91ebf34
Carlos Cortés <ccortes@gmv.com>
2017-11-23 10:48:59 +0100

    Add xib for view

---
+++ ModalStatus/ModalStatus/ModalStatus.h
@@ -0,0 +1,19 @@
+//
+//  ModalStatus.h
+//  ModalStatus
+//
+//  Created by Carlos Cortés Sánchez on 23/11/2017.
+//  Copyright © 2017 Carlos Cortés Sánchez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModalStatus.
+FOUNDATION_EXPORT double ModalStatusVersionNumber;
+
+//! Project version string for ModalStatus.
+FOUNDATION_EXPORT const unsigned char ModalStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModalStatus/PublicHeader.h>
+
+
