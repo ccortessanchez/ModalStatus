c98e03291fef9e73131d79287ca3018ae8a394e8
Carlos Cortés <ccortes@gmv.com>
2017-11-23 11:44:49 +0100

    Animation for view removal

--- ModalStatus/ModalStatus/ModalStatusView.swift
+++ ModalStatus/ModalStatus/ModalStatusView.swift
@@ -42,20 +42,37 @@ public class ModalStatusView: UIView {
         
         headlineLabel.text = ""
         subheadLabel.text = ""
+        
+        contentView.alpha = 0.0
     }
     
     public override func didMoveToSuperview() {
-        // Add a timer to remove the view
-        self.timer = Timer.scheduledTimer(
-            timeInterval: TimeInterval(3.0),
-            target: self,
-            selector: #selector(self.removeSelf),
-            userInfo: nil,
-            repeats: false)
+        // Fade in when added to superview
+        // Then add a timer to remove the view
+        self.contentView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        UIView.animate(withDuration: 0.15, animations: {
+            self.contentView.alpha = 1.0
+            self.contentView.transform = CGAffineTransform.identity
+        }) { _ in
+            self.timer = Timer.scheduledTimer(
+                timeInterval: TimeInterval(3.0),
+                target: self,
+                selector: #selector(self.removeSelf),
+                userInfo: nil,
+                repeats: false)
+        }
     }
     
     @objc private func removeSelf() {
-        self.removeFromSuperview()
+        // Animate removal of view
+        UIView.animate(
+            withDuration: 0.15,
+            animations: {
+                self.contentView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+                self.contentView.alpha = 0.0
+        }) { _ in
+            self.removeFromSuperview()
+        }
     }
     
     // Provide functions to update view
@@ -70,6 +87,15 @@ public class ModalStatusView: UIView {
     
     public func set(subhead text: String) {
         self.subheadLabel.text = text
+    }
+    
+    // Allow view to control itself
+    public override func layoutSubviews() {
+        // Rounded corners
+        self.layoutIfNeeded()
+        self.contentView.layer.masksToBounds = true
+        self.contentView.clipsToBounds = true
+        self.contentView.layer.cornerRadius = 10
     }
     
 }
